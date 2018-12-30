638e70999d5c56e54a328629793ddcbc06a46389
Brian Manthos <manthos@msn.com>
2018-12-30 10:55:34 -0800

    add CANConvert

---
+++ CANConvert/Program.cs
@@ -0,0 +1,32 @@
+﻿using CANLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CANConvert
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      IEnumerable<string> files = Directory.EnumerateFiles(".", "*.csv");
+      foreach (string fileSrc in files)
+      {
+        Console.WriteLine("R {0}", fileSrc);
+        CANLib.CANopen_Magic_Log log = new CANopen_Magic_Log();
+        log.Load(fileSrc);
+
+        Console.WriteLine("C");
+        CANLib.GVRET_Log logConverted = new GVRET_Log(log);
+
+        string fileTgt = @"GVRET\" + Path.GetFileName(fileSrc);
+        Console.WriteLine("W {0}", fileTgt);
+        if (!Directory.Exists("GVRET"))
+        {
+          Directory.CreateDirectory("GVRET");
+        }
+        logConverted.Save(fileTgt);
+      }
+    }
+  }
+}
