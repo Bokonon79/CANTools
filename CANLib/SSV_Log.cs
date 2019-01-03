f22eb2efa108df4fb555ab68ff7ac2d80198044f
Brian Manthos <manthos@msn.com>
2019-01-03 12:18:20 -0800

    CANLib - add SSV_Log

---
+++ CANLib/SSV_Log.cs
@@ -0,0 +1,10 @@
+﻿namespace CANLib
+{
+  public class SSV_Log<T> : DataWithColumns, Log
+    where T : SSV_Log<T>, new()
+  {
+    protected SSV_Log() : base(SeparatorType.Space)
+    {
+    }
+  }
+}
